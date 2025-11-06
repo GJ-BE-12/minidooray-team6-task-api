3a5605108853c7475dd6f6dfeba6ec93c3c37cc2
OiJs <oijs9663@gmail.com>
2025-11-06 14:52:01 +0900

    fix:reposi 수정

--- src/main/java/com/nhnacademy/taskAPI/repository/ProjectMemberRepository.java
+++
@@ -1,5 +0,0 @@
-package com.nhnacademy.taskAPI.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProjectMemberRepository<ProjectMember> extends JpaRepository<ProjectMember, Long>{}
--- src/main/java/com/nhnacademy/taskAPI/service/impl/ProjectServiceImpl.java
+++ src/main/java/com/nhnacademy/taskAPI/service/impl/ProjectServiceImpl.java
@@ -1,1 +1,30 @@
 package com.nhnacademy.taskAPI.service.impl;
+
+import com.nhnacademy.taskAPI.entity.dto.ProjectCreateRequestDto;
+import com.nhnacademy.taskAPI.entity.dto.ProjectResponseDto;
+import com.nhnacademy.taskAPI.service.ProjectService;
+
+import java.util.List;
+
+public class ProjectServiceImpl implements ProjectService{
+
+    @Override
+    public ProjectResponseDto createProject(Long accountId, ProjectCreateRequestDto requestDto) {
+        return null;
+    }
+
+    @Override
+    public List<ProjectResponseDto> findProjectsByAccountId(Long accountId) {
+        return List.of();
+    }
+
+    @Override
+    public ProjectResponseDto findProjectDetails(Long accountId, Long projectId) {
+        return null;
+    }
+
+    @Override
+    public void addMemberToProject(Long adminAccountId, Long projectId, Long accountIdToAdd) {
+
+    }
+}
